b4627e58df4974de3821495ff66b92cec5b301cf
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-11-22 20:20:15 +0530

    conversion operation

---
+++ conversionOperations.js
@@ -0,0 +1,41 @@
+let score = "abhishek";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "abhishek";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abhishek" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
