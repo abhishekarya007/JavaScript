ae428dedce0ca5a1ce19acf79dcbf9027f628b63
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-11-23 20:18:15 +0530

    color changer

---
+++ 05_projects/Color-Changer/colorChanger.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document.body.style.backgroundColor = e.target.id;
+  });
+});
