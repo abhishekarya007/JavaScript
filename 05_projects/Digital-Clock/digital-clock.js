e66b1445fd796a8f042ecb5a731609db047eea6b
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-11-24 15:25:29 +0530

    digital clock clearInterval added

--- 05_projects/Digital-Clock/digital-clock.js
+++ 05_projects/Digital-Clock/digital-clock.js
@@ -1,12 +1,17 @@
-setInterval(() => {
+const clock = document.querySelector("#clock");
+
+const a = setInterval(() => {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  const clock = document.querySelector("#clock");
 
   //   clock.innerHTML = date.toLocaleTimeString();
   const timing = hours > 12 ? " PM" : " AM";
 
   clock.innerHTML = `${hours}:${minutes}:${seconds}` + timing;
 }, 1000);
+
+clock.addEventListener("click", () => {
+  clearInterval(a);
+});
