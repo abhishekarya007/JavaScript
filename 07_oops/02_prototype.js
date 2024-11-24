1f436838b2f660d0302d39b3551e6cfbfd52b604
abhishekarya007 <abhishek.kumarst@gmail.com>
2024-11-24 23:06:31 +0530

    notes for prototype

--- 07_oops/02_prototype.js
+++ 07_oops/02_prototype.js
@@ -1,3 +1,12 @@
+//what is prototype?
+//Prototypes are the mechanism by which JavaScript objects inherit features from one another.
+//Every object in JavaScript has a built-in property, which is called its prototype.
+//The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype chain.
+// The chain ends when we reach a prototype that has null for its own prototype.
+
+//When you try to access a property of an object: if the property can't be found in the object itself, the prototype is searched for the property.
+//If the property still can't be found, then the prototype's prototype is searched, and so on until either the property is found, or the end of the chain is reached, in which case undefined is returned.
+
 //reference
 //Js goes from parent to grandparent to grand-grand-parent... to look for methods / variables
 
@@ -36,3 +45,15 @@ String.prototype.trueLength = function () {
 anotherUsername.trueLength();
 "hitesh".trueLength();
 "iceTea".trueLength();
+
+const Teacher = {
+  makeVideo: true,
+};
+
+const TeachingSupport = {
+  isAvailable: false,
+};
+
+// modern syntax Inheritece
+Object.setPrototypeOf(TeachingSupport, Teacher); // TeachingSupport inherites from Teacher
+console.log(TeachingSupport.makeVideo);
